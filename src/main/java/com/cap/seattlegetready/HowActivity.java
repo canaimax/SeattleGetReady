9e4e5fe5d91cb87d3133d5a4b28cde3a3a466566
Captain <44041524+canaimax@users.noreply.github.com>
2019-11-24 23:22:26 -0800

    UPDATE Four

--- src/main/java/com/cap/seattlegetready/HowActivity.java
+++ src/main/java/com/cap/seattlegetready/HowActivity.java
@@ -1,11 +1,13 @@
 package com.cap.seattlegetready;
 
+import android.content.Intent;
 import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class HowActivity extends AppCompatActivity {
 
@@ -15,6 +17,43 @@ public class HowActivity extends AppCompatActivity {
         setContentView(R.layout.activity_how);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+
+        Button stayingButton = (Button) findViewById(R.id.stayingsafe);
+        stayingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HowActivity.this, StaySafeAct.class));
+            }
+        });
+
+        Button buildingButton = (Button) findViewById(R.id.buildINGakit);
+        buildingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HowActivity.this, BuildKitAct.class));
+            }
+        });
+
+
+        Button storingButton = (Button) findViewById(R.id.storingwater);
+        storingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HowActivity.this, StoreWaterAct.class));
+            }
+        });
+
+        Button medicalButton = (Button) findViewById(R.id.medicalneeds);
+        medicalButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HowActivity.this, MedNeedsAct.class));
+            }
+        });
+
+
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
